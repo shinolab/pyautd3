02c54f737de4318a9c83d48e7a85b4c3df5a48d4
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-01-24 13:29:06 +0900

    lint example

--- example/samples/flag.py
+++ example/samples/flag.py
@@ -25,7 +25,7 @@ async def flag(autd: Controller) -> None:
     while not fin:
         states = await autd.fpga_state_async()
         print(f"{prompts[(prompts_idx // 1000) % len(prompts)]} FPGA Status...")
-        print("\n".join([f"\x1b[0K[{i}]: thermo = {"-" if state is None else state.is_thermal_assert()}" for i, state in enumerate(states)]))
+        print("\n".join([f"\x1b[0K[{i}]: thermo = {'-' if state is None else state.is_thermal_assert()}" for i, state in enumerate(states)]))
         print(f"\x1b[{len(states) + 1}A", end="")
         prompts_idx += 1
     print("\x1b[1F\x1b[0J")
