7bb0e7b3337752f36378e80b1addb805159460cb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-02-20 09:35:24 +0900

    update `FPGAState`

--- pyautd3/driver/fpga/defined/fpga_state.py
+++ pyautd3/driver/fpga/defined/fpga_state.py
@@ -1,5 +1,7 @@
 import ctypes
 
+from pyautd3.native_methods.autd3capi_def import Segment
+
 
 class FPGAState:
     """FPGA information."""
@@ -11,7 +13,42 @@ class FPGAState:
 
     def is_thermal_assert(self: "FPGAState") -> bool:
         """Check if thermal sensor is asserted."""
-        return (int(self.info) & 0x01) != 0
+        return (int(self.info) & (1 << 0)) != 0
 
-    def __str__(self: "FPGAState") -> str:
-        return f"Thermal assert = {self.is_thermal_assert()}"
+    def current_mod_segment(self: "FPGAState") -> Segment:
+        """Get current modulation segment."""
+        match int(self.info) & (1 << 1):
+            case 0:
+                return Segment.S0
+            case _:
+                return Segment.S1
+
+    def current_stm_segment(self: "FPGAState") -> Segment | None:
+        """Get current STM segment."""
+        if not self.is_stm_mode():
+            return None
+
+        match int(self.info) & (1 << 2):
+            case 0:
+                return Segment.S0
+            case _:
+                return Segment.S1
+
+    def current_gain_segment(self: "FPGAState") -> Segment | None:
+        """Get current gain segment."""
+        if not self.is_gain_mode():
+            return None
+
+        match int(self.info) & (1 << 2):
+            case 0:
+                return Segment.S0
+            case _:
+                return Segment.S1
+
+    def is_gain_mode(self: "FPGAState") -> bool:
+        """Check if gain mode."""
+        return (int(self.info) & (1 << 3)) != 0
+
+    def is_stm_mode(self: "FPGAState") -> bool:
+        """Check if STM mode."""
+        return not self.is_gain_mode()
