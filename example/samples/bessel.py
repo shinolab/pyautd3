3ab2fed3c08a8f593b1f0899bc6b47047c304d92
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-05-22 13:59:38 +0900

    fix examples

--- example/samples/bessel.py
+++ example/samples/bessel.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-from pyautd3 import Bessel, Controller, Hz, Silencer, Sine
+from pyautd3 import Bessel, Controller, Hz, Silencer, Sine, rad
 
 
 def bessel(autd: Controller) -> None:
     config = Silencer.default()
     autd.send(config)
 
-    f = Bessel(autd.geometry.center, np.array([0.0, 0.0, 1.0]), 13.0 / 180 * np.pi)
+    f = Bessel(autd.geometry.center, np.array([0.0, 0.0, 1.0]), 13.0 / 180 * np.pi * rad)
     m = Sine(150 * Hz)
 
     autd.send(m, f)
--- example/samples/custom.py
+++ example/samples/custom.py
@@ -1,3 +1,5 @@
+from collections.abc import Callable
+
 from pyautd3 import Controller, Device, Drive, EmitIntensity, Hz, Phase, Silencer, Sine, Transducer
 from pyautd3.gain import Custom
 
@@ -6,14 +8,8 @@ def transtest(autd: Controller) -> None:
     config = Silencer.default()
     autd.send(config)
 
-    def f(dev: Device, tr: Transducer) -> Drive:
-        match (dev.idx, tr.idx):
-            case (0, 0):
-                return Drive(Phase(0), EmitIntensity.maximum())
-            case (0, 248):
-                return Drive(Phase(0), EmitIntensity.maximum())
-            case _:
-                return Drive.null()
+    def f(dev: Device) -> Callable[[Transducer], Drive]:
+        return lambda tr: Drive(Phase(0), EmitIntensity.maximum()) if dev.idx == 0 and tr.idx in (0, 248) else Drive.null()
 
     g = Custom(f)
     m = Sine(150 * Hz)
