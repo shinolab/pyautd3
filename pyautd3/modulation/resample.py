c3f1ab874065c173947772fca8633d8dc6749e19
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-03 19:19:33 +0900

    add type annotation for resampler

--- pyautd3/modulation/resample.py
+++ pyautd3/modulation/resample.py
@@ -7,27 +7,27 @@ from pyautd3.native_methods.autd3capi_driver import DynSincInterpolator, DynWind
 
 class Resampler(metaclass=ABCMeta):
     @abstractmethod
-    def _dyn_resampler(self) -> DynSincInterpolator:
+    def _dyn_resampler(self: "Resampler") -> DynSincInterpolator:
         pass
 
 
 class BlackMan:
     _window_size: int
 
-    def __init__(self, window_size: int) -> None:
+    def __init__(self: "BlackMan", window_size: int) -> None:
         self._window_size = window_size
 
-    def _window(self) -> ctypes.c_uint32:
+    def _window(self: "BlackMan") -> ctypes.c_uint32:
         return ctypes.c_uint32(int(DynWindow.Blackman))
 
 
 class Rectangular:
     _window_size: int
 
-    def __init__(self, window_size: int) -> None:
+    def __init__(self: "Rectangular", window_size: int) -> None:
         self._window_size = window_size
 
-    def _window(self) -> ctypes.c_uint32:
+    def _window(self: "Rectangular") -> ctypes.c_uint32:
         return ctypes.c_uint32(int(DynWindow.Rectangular))
 
 
@@ -43,5 +43,5 @@ class SincInterpolation(
     def __init__(self: "SincInterpolation", window: T | None = None) -> None:
         self._window = window if window is not None else BlackMan(32)
 
-    def _dyn_resampler(self) -> DynSincInterpolator:
+    def _dyn_resampler(self: "SincInterpolation") -> DynSincInterpolator:
         return DynSincInterpolator(self._window._window(), ctypes.c_uint32(self._window._window_size))
