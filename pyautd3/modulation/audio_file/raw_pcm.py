f2d0e81190decdf07740dd07a7827fbcebabf088
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-15 09:52:00 +0900

    rename from `new_with_resampler` to `new_with_resample`

--- pyautd3/modulation/audio_file/raw_pcm.py
+++ pyautd3/modulation/audio_file/raw_pcm.py
@@ -34,7 +34,7 @@ class RawPCM(
         self.__private_init__(path, SamplingConfig(config))
 
     @staticmethod
-    def new_with_resampler(
+    def new_with_resample(
         path: Path,
         source: Freq[float],
         target: SamplingConfig | Freq[int] | Freq[float] | timedelta,
