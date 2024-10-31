b643665e4e65463611ab34c504c2c51a7f5150a8
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-31 12:44:35 +0900

    bump to 29.0.0rc3.post4

--- pyautd3/link/soem/remote.py
+++ pyautd3/link/soem/remote.py
@@ -5,7 +5,7 @@ from pyautd3.native_methods.autd3capi import ControllerPtr
 from pyautd3.native_methods.autd3capi import NativeMethods as Base
 from pyautd3.native_methods.autd3capi_driver import HandlePtr, LinkBuilderPtr, LinkPtr
 from pyautd3.native_methods.autd3capi_link_soem import NativeMethods as LinkSOEM
-from pyautd3.native_methods.utils import _validate_ptr
+from pyautd3.native_methods.utils import _to_null_terminated_utf8, _validate_ptr
 
 
 class _RemoteSOEMBuilder(LinkBuilder["RemoteSOEM"]):
@@ -15,7 +15,7 @@ class _RemoteSOEMBuilder(LinkBuilder["RemoteSOEM"]):
         self.addr = addr
 
     def _link_builder_ptr(self: Self) -> LinkBuilderPtr:
-        return _validate_ptr(LinkSOEM().link_remote_soem(self.addr.encode("utf-8")))  # pragma: no cover
+        return _validate_ptr(LinkSOEM().link_remote_soem(_to_null_terminated_utf8(self.addr)))  # pragma: no cover
 
     def _resolve_link(self: Self, handle: HandlePtr, ptr: ControllerPtr) -> "RemoteSOEM":
         return RemoteSOEM(handle, Base().link_get(ptr))  # pragma: no cover
