81f05581b1f115486196d93fe2f1f8462cd247c4
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-11-03 10:07:33 +0900

    rm soem

--- tools/wrapper-generator/src/python.rs
+++ tools/wrapper-generator/src/python.rs
@@ -210,7 +210,6 @@ impl PythonGenerator {
                     | "AUTDInternalError"
                     | "FirmwareVersionType"
                     | "TypeTag"
-                    | "SOEMError"
             )
         }));
         self
