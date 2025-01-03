7012c391b1939fa7ee26ffbf3bb68fb88ef7eccb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-01-03 10:25:54 +0900

    use builder decorator

--- pyautd3/gain/uniform.py
+++ pyautd3/gain/uniform.py
@@ -1,6 +1,7 @@
 from typing import Self
 
 from pyautd3.derive import datagram, gain
+from pyautd3.derive.derive_builder import builder
 from pyautd3.derive.derive_datagram import datagram_with_segment
 from pyautd3.driver.datagram.gain import Gain
 from pyautd3.driver.firmware.fpga.drive import Drive
@@ -11,19 +12,16 @@ from pyautd3.native_methods.autd3capi import NativeMethods as Base
 from pyautd3.native_methods.autd3capi_driver import GainPtr
 
 
+@builder
 @gain
 @datagram
 @datagram_with_segment
 class Uniform(Gain):
-    _drive: Drive
+    _prop_drive: Drive
 
     def __init__(self: Self, drive: Drive | EmitIntensity | Phase | tuple) -> None:
         super().__init__()
-        self._drive = Drive(drive)
-
-    @property
-    def drive(self: Self) -> Drive:
-        return self._drive
+        self._prop_drive = Drive(drive)
 
     def _gain_ptr(self: Self, _: Geometry) -> GainPtr:
-        return Base().gain_uniform(self._drive._intensity.value, self._drive._phase.value)
+        return Base().gain_uniform(self._prop_drive.intensity.value, self._prop_drive.phase.value)
