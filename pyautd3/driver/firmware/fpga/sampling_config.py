a6b4ae3753214927bc491f5e56f4214d597ab284
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-02-24 12:50:16 +0900

    update tests

--- pyautd3/driver/firmware/fpga/sampling_config.py
+++ pyautd3/driver/firmware/fpga/sampling_config.py
@@ -2,7 +2,6 @@ from typing import Self
 
 from pyautd3.driver.defined import Freq
 from pyautd3.driver.defined.freq import Hz
-from pyautd3.driver.utils import _validate_nonzero_u16
 from pyautd3.native_methods.autd3capi import NativeMethods as Base
 from pyautd3.native_methods.autd3capi import SamplingConfigWrap
 from pyautd3.native_methods.utils import _validate_duration, _validate_f32, _validate_sampling_config, _validate_u16
@@ -15,7 +14,9 @@ class SamplingConfig:
     def __init__(self: Self, value: "SamplingConfigWrap | SamplingConfig | int | Freq[int] | Freq[float] | Duration") -> None:
         match value:
             case int():
-                self._inner = _validate_sampling_config(Base().sampling_config_from_division(_validate_nonzero_u16(value)))
+                if value > 0xFFFF:  # noqa: PLR2004
+                    raise ValueError
+                self._inner = _validate_sampling_config(Base().sampling_config_from_division(value))
             case SamplingConfigWrap():
                 self._inner = value
             case SamplingConfig():
