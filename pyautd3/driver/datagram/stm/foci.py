9eb56e8e95199f4f5abe3d2df2b3ecbc55506971
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-02-25 11:28:34 +0900

    refactor `ControlPoints`

--- pyautd3/driver/datagram/stm/foci.py
+++ pyautd3/driver/datagram/stm/foci.py
@@ -1,12 +1,14 @@
 import ctypes
 from collections.abc import Iterable
-from typing import Generic, Self, TypeVar
+from typing import Self
 
 import numpy as np
 from numpy.typing import ArrayLike
 
 from pyautd3.driver.datagram.datagram import Datagram
 from pyautd3.driver.datagram.stm.control_point import (
+    ControlPoint,
+    ControlPoints,
     ControlPoints1,
     ControlPoints2,
     ControlPoints3,
@@ -15,7 +17,6 @@ from pyautd3.driver.datagram.stm.control_point import (
     ControlPoints6,
     ControlPoints7,
     ControlPoints8,
-    IControlPoints,
 )
 from pyautd3.driver.datagram.stm.stm_sampling_config import FreqNearest, PeriodNearest, _sampling_config
 from pyautd3.driver.datagram.with_loop_behavior import DatagramL
@@ -31,32 +32,15 @@ from pyautd3.utils import Duration
 
 __all__ = []  # type: ignore[var-annotated]
 
-C = TypeVar("C", bound=IControlPoints)
 
-
-class FociSTM(
-    DatagramS[FociSTMPtr],
-    DatagramL[FociSTMPtr],
-    Datagram,
-    Generic[C],
-):
-    foci: list[C]
+class FociSTM(DatagramS[FociSTMPtr], DatagramL[FociSTMPtr], Datagram):
+    foci: list[ControlPoints]
     config: SamplingConfig | Freq[float] | Duration | FreqNearest | PeriodNearest
 
     @classmethod
     def __private_new__(
         cls: type["FociSTM"],
-        foci: (
-            Iterable[ArrayLike]
-            | Iterable[ControlPoints1]
-            | Iterable[ControlPoints2]
-            | Iterable[ControlPoints3]
-            | Iterable[ControlPoints4]
-            | Iterable[ControlPoints5]
-            | Iterable[ControlPoints6]
-            | Iterable[ControlPoints7]
-            | Iterable[ControlPoints8]
-        ),
+        foci: (Iterable[ArrayLike] | Iterable[ControlPoint] | Iterable[ControlPoints]),
         config: SamplingConfig | Freq[float] | Duration | FreqNearest | PeriodNearest,
     ) -> "FociSTM":
         ins = super().__new__(cls)
@@ -65,50 +49,23 @@ class FociSTM(
 
     def __private_init__(
         self: "FociSTM",
-        foci: (
-            Iterable[ArrayLike]
-            | Iterable[ControlPoints1]
-            | Iterable[ControlPoints2]
-            | Iterable[ControlPoints3]
-            | Iterable[ControlPoints4]
-            | Iterable[ControlPoints5]
-            | Iterable[ControlPoints6]
-            | Iterable[ControlPoints7]
-            | Iterable[ControlPoints8]
-        ),
+        foci: (Iterable[ArrayLike] | Iterable[ControlPoint] | Iterable[ControlPoints]),
         config: SamplingConfig | Freq[float] | Duration | FreqNearest | PeriodNearest,
     ) -> None:
-        foci = list(foci)
-        match foci[0]:
-            case (
-                ControlPoints1()
-                | ControlPoints2()
-                | ControlPoints3()
-                | ControlPoints4()
-                | ControlPoints5()
-                | ControlPoints6()
-                | ControlPoints7()
-                | ControlPoints8()
-            ):
-                self.foci = foci
+        foci_ = list(foci)
+        match foci_[0]:
+            case ControlPoints():
+                self.foci = foci_  # type: ignore[assignment]
+            case ControlPoint():
+                self.foci = [ControlPoints(points=[p]) for p in foci_]  # type: ignore[list-item, arg-type]
             case _:
-                self.foci = [ControlPoints1(points=p) for p in foci]
+                self.foci = [ControlPoints(points=[ControlPoint(point=p)]) for p in foci_]  # type: ignore[arg-type]
 
         self.config = config
 
     def __init__(
         self: "FociSTM",
-        foci: (
-            Iterable[ArrayLike]
-            | Iterable[ControlPoints1]
-            | Iterable[ControlPoints2]
-            | Iterable[ControlPoints3]
-            | Iterable[ControlPoints4]
-            | Iterable[ControlPoints5]
-            | Iterable[ControlPoints6]
-            | Iterable[ControlPoints7]
-            | Iterable[ControlPoints8]
-        ),
+        foci: (Iterable[ArrayLike] | Iterable[ControlPoint] | Iterable[ControlPoints]),
         config: SamplingConfig | Freq[float] | Duration,
     ) -> None:
         self.__private_init__(foci, config)
@@ -123,17 +80,53 @@ class FociSTM(
                 raise TypeError
 
     def _raw_ptr(self: Self, _: Geometry) -> FociSTMPtr:
-        n = self.foci[0]._value()
-        foci = np.fromiter((np.void(p) for p in self.foci), dtype=np.dtype((np.void, 4 + n * 16)))  # type: ignore[type-var,call-overload]
+        n = self._n()
+        match n:
+            case 1:
+                foci = (ControlPoints1(p.points[0], p.intensity.value) for p in self.foci)
+            case 2:
+                foci = (ControlPoints2(p.points[0], p.points[1], p.intensity.value) for p in self.foci)  # type: ignore[misc]
+            case 3:
+                foci = (ControlPoints3(p.points[0], p.points[1], p.points[2], p.intensity.value) for p in self.foci)  # type: ignore[misc]
+            case 4:
+                foci = (ControlPoints4(p.points[0], p.points[1], p.points[2], p.points[3], p.intensity.value) for p in self.foci)  # type: ignore[misc]
+            case 5:
+                foci = (ControlPoints5(p.points[0], p.points[1], p.points[2], p.points[3], p.points[4], p.intensity.value) for p in self.foci)  # type: ignore[misc]
+            case 6:
+                foci = (
+                    ControlPoints6(p.points[0], p.points[1], p.points[2], p.points[3], p.points[4], p.points[5], p.intensity.value)  # type: ignore[misc]
+                    for p in self.foci
+                )
+            case 7:
+                foci = (
+                    ControlPoints7(p.points[0], p.points[1], p.points[2], p.points[3], p.points[4], p.points[5], p.points[6], p.intensity.value)  # type: ignore[misc]
+                    for p in self.foci
+                )
+            case _:
+                foci = (
+                    ControlPoints8(  # type: ignore[misc]
+                        p.points[0],
+                        p.points[1],
+                        p.points[2],
+                        p.points[3],
+                        p.points[4],
+                        p.points[5],
+                        p.points[6],
+                        p.points[7],
+                        p.intensity.value,
+                    )
+                    for p in self.foci
+                )
+        foci_ = np.fromiter((np.void(p) for p in foci), dtype=np.dtype((np.void, 4 + n * 16)))  # type: ignore[type-var,call-overload]
         return Base().stm_foci(
             self.sampling_config()._inner,
-            foci.ctypes.data_as(ctypes.c_void_p),  # type: ignore[arg-type]
+            foci_.ctypes.data_as(ctypes.c_void_p),  # type: ignore[arg-type]
             len(self.foci),
             n,
         )
 
     def _into_segment(self: Self, ptr: FociSTMPtr, segment: Segment, transition_mode: TransitionModeWrap | None) -> DatagramPtr:
-        return Base().stm_foci_into_datagram_with_segment(ptr, self.foci[0]._value(), segment, transition_mode or TransitionMode.NONE)
+        return Base().stm_foci_into_datagram_with_segment(ptr, self._n(), segment, transition_mode or TransitionMode.NONE)
 
     def _into_loop_behavior(
         self: Self,
@@ -144,14 +137,25 @@ class FociSTM(
     ) -> DatagramPtr:
         return Base().stm_foci_into_datagram_with_loop_behavior(
             ptr,
-            self.foci[0]._value(),
+            self._n(),
             segment,
             transition_mode or TransitionMode.NONE,
             loop_behavior,
         )
 
     def _datagram_ptr(self: Self, geometry: Geometry) -> DatagramPtr:
-        return Base().stm_foci_into_datagram(self._raw_ptr(geometry), self.foci[0]._value())
+        return Base().stm_foci_into_datagram(self._raw_ptr(geometry), self._n())
 
     def sampling_config(self: Self) -> SamplingConfig:
         return _sampling_config(self.config, len(self.foci))
+
+    def _n(self: Self) -> int:
+        n = len(self.foci[0].points)
+        if any(len(f.points) != n for f in self.foci):
+            msg = "All components must have the same number of foci"
+            raise ValueError(msg)
+        match n:
+            case v if 0 < v <= 8:  # noqa: PLR2004
+                return n
+            case _:
+                raise ValueError
