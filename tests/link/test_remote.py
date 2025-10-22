63faf719c884740c3806549f7511315dc64c1efb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-22 11:45:34 +0900

    add tests

--- tests/link/test_remote.py
+++ tests/link/test_remote.py
@@ -1,4 +1,16 @@
+from pyautd3 import Duration
 from pyautd3.link.remote import Remote, RemoteOption
+
+
+def test_option():
+    option = RemoteOption()
+    inner = option._inner()
+    assert not inner.timeout.has_value
+
+    option = RemoteOption(timeout=Duration.from_nanos(0))
+    inner = option._inner()
+    assert inner.timeout.has_value
+    assert inner.timeout.value == Duration.from_nanos(0)._inner
 
 
 def test_remote():
