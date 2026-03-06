92e9e07e490f749ce1bcfc50b93e557bbdf8efb4
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2026-03-06 09:43:29 +0900

    perf: improve custom gain performance
    refactor: remove unused Transducer cache in Custom class

--- pyautd3/gain/custom.py
+++ pyautd3/gain/custom.py
@@ -14,10 +14,24 @@ class Custom(Gain):
     def __init__(self: Self, f: Callable[[Device], Callable[[Transducer], Drive]]) -> None:
         super().__init__()
 
+        dev_cache: dict[int, Device] = {}
+        fn_cache: dict[int, Callable[[Transducer], Drive]] = {}
+
         def f_native(_context: ConstPtr, geometry_ptr: GeometryPtr, dev_idx: int, tr_idx: int, raw) -> None:  # noqa: ANN001
-            dev = Device(dev_idx, geometry_ptr)
+
+            dev = dev_cache.get(dev_idx)
+            if dev is None:
+                dev = Device(dev_idx, geometry_ptr)
+                dev_cache[dev_idx] = dev
+
+            fn = fn_cache.get(dev_idx)
+            if fn is None:
+                fn = f(dev)
+                fn_cache[dev_idx] = fn
+
             tr = Transducer(tr_idx, dev_idx, dev._ptr)
-            d = f(dev)(tr)
+
+            d = fn(tr)
             raw[0] = Drive_(d.phase._inner(), d.intensity._inner())
 
         self._f_native = ctypes.CFUNCTYPE(None, ConstPtr, GeometryPtr, ctypes.c_uint16, ctypes.c_uint8, ctypes.POINTER(Drive_))(f_native)
